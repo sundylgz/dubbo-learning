258f9b4311d39c6a9224c27332f32dd8530c6741
sundylgz <1159279222@qq.com>
2018-11-11 19:17:04 +0800

    添加配置

---
+++ boot-backend-web/src/main/java/com/sundy/bootbackendweb/BootBackendWebApplication.java
@@ -0,0 +1,19 @@
+package com.sundy.bootbackendweb;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.sundy.ac.api.AcService;
+import com.sundy.ac.po.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@EnableDubbo
+@SpringBootApplication
+public class BootBackendWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBackendWebApplication.class, args);
+    }
+}
